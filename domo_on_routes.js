67fb835f8fce4adadd0df2194e5725acbb4e9314
Mathias Prinz <mprinz@littleweblab.com>
2012-11-28 16:56:59 +0100

    new version without section wrapper

--- domo_on_routes.js
+++ domo_on_routes.js
@@ -2,70 +2,95 @@
 // (c) 2012 Mathias Prinz
 // is distributed under the MIT license.
 
-!function() {
+
+
+! function () {
 
    typeof module == "object"
-       ? module.exports = Route 
-       : window.ROUTE = Route 
-       ;
+        ? module.exports = Route 
+        : window.ROUTE = Route 
+        ;
 
    function Route () {
    
       var concat = Array.prototype.concat
         , childNodes = concat.apply( [], arguments )
         , attributes = childNodes.shift()
-        , section = domo.SECTION()
-        , routeObjects = initRouteObjs()
+        , lookup 
         ;
       
-      function initRouteObjs ( routeObjs ) {
-         
-         routeObjs = routeObjs || [ { frag:[], dom: section } ]
-         
-         var obj = routeObjs[ routeObjs.length - 1 ]
-           , child = childNodes.shift()
-           , isRoute = child instanceof RegExp
-           ;
-         
-         if ( isRoute ) { routeObjs.push( { route: child, dom: section, frag: [] } ); }
-         
-         else { obj.frag.push( child ); }
-         
-         if ( ! childNodes.length ) { return routeObjs; } 
-         
-         return arguments.callee.call( null, routeObjs );
-         
+      function matches ( regEx ) { 
+         var url = typeof attributes === 'string' ?  attributes : attributes.url;
+         if ( url.apply ) { url = url(); }
+         return url.search( regEx ) > -1;
       }
       
-      function routing ( routeObjs, before, path  ) {
-      
-         if ( ! routeObjs.length ) {
+      function routing ( childs, matched ) {
          
-            if ( section.childNodes.length ) { section.innerHTML = ''; }
-            section.appendChild( domo.FRAGMENT.apply( domo, concat.apply( before, path ) ) );
-            return section;
+         if ( ! childs.length ) {
+            matched = matched || { frag:[ domo.TEXT('') ] };
          }
-   
-         before = before || []; path = path || [];
-   
-         var url = typeof attributes === 'string' ?  attributes : attributes.url
-           , obj = routeObjs.shift()
+         
+         var child = childs.shift()
+           , isRegEx = ! child ? true : child instanceof RegExp
            ;
          
-         if ( url.apply ) url = url();
+         if ( matched && ! isRegEx ) {
+            matched.frag.push( child );
+         } 
          
-         if ( ! obj.route ) { before = obj.frag; }
-   
-         if (  url.search( obj.route ) > -1 ) { path = obj.frag; }
-   
-         return arguments.callee.call( null, routeObjs, before, path );
-   
+         if ( matched && isRegEx || matched && ! childs.length  ) {
+            
+            
+            if ( ! lookup ) {
+              
+             var frag = domo.FRAGMENT.apply( domo, concat.apply( [], matched.frag ) ) 
+              lookup = {}
+              lookup[ matched.route ] = {  route: matched.route, children: concat.apply( [], frag.childNodes ), frag: frag  }; 
+              lookup.current = lookup[ matched.route ] 
+              return lookup[ matched.route ].frag;
+               
+            } 
+            else  {
+               if ( matched.route ===  lookup.current.route  ) return;
+               
+               var frag = lookup[ matched.route ] && lookup[ matched.route ].frag ? lookup[ matched.route ].frag : domo.FRAGMENT.apply( domo, concat.apply( [], matched.frag ) );
+               var parent = lookup.current.children[ 0 ].parentNode; 
+               var first = lookup.current.children[ 0 ]
+               
+               lookup[ matched.route ] = { route: matched.route, children: concat.apply( [], frag.childNodes ) , frag: frag }; 
+               
+               parent.insertBefore( frag, first )
+               
+               lookup.current.frag = domo.FRAGMENT.apply( domo, lookup.current.children  );
+                 
+               lookup.current = lookup[ matched.route ] 
+              
+            }
+            
+            return;
+  
+         }
+          
+        
+         
+         if ( isRegEx && matches( child ) && ! matched ) {
+            matched = { route: child, frag: [] }
+         }
+         
+     
+         
+         return arguments.callee( childs, matched );
+         
+      
       }
-   
+      
       if ( attributes.routing ) { 
-         attributes.routing( function () { return routing.call( null, routeObjects.slice() ); } ); 
+         attributes.routing( function () { return routing.call( null, childNodes.slice()); } ); 
       }
-   
-      return routing.call( null, routeObjects.slice() );
+      
+      return routing.call( null, childNodes.slice() );
+      
+
    }
 }()
--- index.js
+++ index.js
@@ -2,13 +2,15 @@ require('domo').global();
 
 var ROUTE = require('./domo_on_routes')
   , http = require('http')
+  , oscarsSentence = 'Oscar Said:, Be,yourself,everyone,else,is,taken,!!!'.split(',')
+  , oscarSaid = function() { return H2( oscarsSentence.shift() || 'Oscar is done!' ); }
   ;
-
-http.createServer(function(req, res) {
   
+http.createServer(function( req, res ) {
+ console.log( req.url  )
+  if ( req.url  === '/favicon.ico') res.end('');
   res.writeHead(200, {"Content-Type": "text/html"})
   res.end( 
-    
     DOCUMENT(
       HTML({ lang: 'en' }
       ,
@@ -19,11 +21,26 @@ http.createServer(function(req, res) {
           ,  
             STYLE.on( 'body', { textAlign: "center", fontSize: 24 } )
             ,
-            STYLE.on( 'h1', { backgroundColor: 'yellow' } )
+            ROUTE( req.url
             ,
+              /yellow/
+              ,
+              STYLE.on( 'h1', { backgroundColor: 'yellow' } )
+              ,
+              /blue/
+              ,
+              STYLE.on( 'h1', { backgroundColor: 'blue' } )
+              ,
+              /pink/
+              ,
+              STYLE.on( 'h1', { backgroundColor: 'pink' } )
+              
+          )
+            ,
+            
             STYLE.on( 'h2', { backgroundColor: 'grey' } )
           )//STYLE
-        )//HEAD
+          )//HEAD
         ,
         BODY(
           A( { href: '/yellow' }, 'Yellow' )
@@ -40,17 +57,21 @@ http.createServer(function(req, res) {
           ,
             /yellow/
             , 
+              oscarSaid
+              ,
               H1( 'Yellow' )
               ,
             /blue/
             ,
-              H1( 'Blue' )
+              oscarSaid
               ,
-              A( { href: 'blue/light' }, 'Light Blue' )
+              H1( 'Blue' ) 
+              ,
+              A( { href: '/blue/light' }, 'Light Blue' )
               ,
               ' | '
               ,
-              A({ href: 'blue/dark' }, 'Dark Blue' )
+              A({ href: '/blue/dark' }, 'Dark Blue' )
               ,
               ROUTE( req.url
               ,
@@ -65,13 +86,16 @@ http.createServer(function(req, res) {
               ,
             /pink/
             ,
-            H1( 'Pink' )
+              oscarSaid
+              ,
+              H1( 'Pink' )
           )//ROUTE
         )//BODY 
       )//HTML
     ).outerHTML 
+  
   );
 
 }).listen( 8080 );
 
-console.log( 'Hello :) Domo is routing now!' );
+console.log( 'Hello :) Domo is routing now on :8080 !' );
